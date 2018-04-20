62b084d745e3b341a48b6dc1e55a4d36cc98cf48
William Will <will.e.gallagher@gmail.com>
2018-04-20 06:03:16 -0500

    Refined version of my first attempt at foreach.
    I created this program to do a simple check of a word against a small dictionary.

--- foreach_test/Program.cs
+++ foreach_test/Program.cs
@@ -8,25 +8,36 @@ namespace foreach_test
         {
             Console.WriteLine("Hello World!\n");
 
-            //  Array for whitelist check
+            //  Array for whitelist check - this may later be a class and several objects:
             string[] word_list = new string[] { "a", "ab", "about", "at", "end", "word" };
 
+            //  First word to check - replace with user input:
             string word = "banana";
 
+            //  Check the words for matches:
             void check_word(string f)
             {
-                int depth = 0;
+                //  This will let us know when we have checked the entire list of words and later, links:
+                int depth = 0;  //  Also, this resets for each word searched.
+                
+                //  This was easier to write, I guess:
                 int total_words = word_list.Length;
 
+                //  For each word in the word list, check if the input word matches:
                 foreach (string r in word_list)
-                {
+                {  
+                    //  This will keep cound of how many words we have looked at.
                     depth += 1;
-                    if (r == word)
+                    
+                    //  If our supplied word matches one of the words in our dictionary...:
+                    if (word == r)
                     {
                         Console.WriteLine("Success! Message approved!" + "\n");
-                        break;
+                        break; //   Exit loop and begin next part of the program.
                     }
-                    if((depth == total_words))
+                    
+                    //  If we reached the end of the list and still don't have a match...:
+                    if((depth == total_words) && (word != r))
                     {
                         Console.WriteLine("One of your words didn't pass inspection.\n");
                     }
@@ -37,10 +48,11 @@ namespace foreach_test
             //  Testing error message
             check_word(word);
 
-            //  "Success!" Message.
+            //  Testing "Success!" Message.
             word = "word";
             check_word(word);
 
+            //  Keep the window open until you press "Enter" (thanks @ https://stackoverflow.com/a/8868363/8233382):
             Console.WriteLine("Press enter to close...");
             Console.ReadLine();
         }
